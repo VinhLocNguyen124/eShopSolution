dbad3d55ee14c0a152604e7b70c88adfe1c4a134
Vinh Loc Nguyen <49594321+VinhLocNguyen124@users.noreply.github.com>
2021-10-16 23:49:27 +0700

    Delete one prop from Model

--- eShopSolution.WebApp/Models/ErrorViewModel.cs
+++ eShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -5,8 +5,7 @@ namespace eShopSolution.WebApp.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-        public int MyProperty { get; set; }
-
+   
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
